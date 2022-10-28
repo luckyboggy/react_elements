86e2edff1519459b362081c41e39d5b1cbc89d96
Ковалёв Богдан Олегович <kovalev-bo@ais-npp.local>
2022-10-28 14:12:42 +0500

    quiz_start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './index.scss';
 function App() {
   return (
     <div className="App">
-
+      <div className="progress">
+        <div className="progress_inner"></div>
+      </div>
+      <h1>Что такое useState</h1>
+      <ul>
+        <li>Это функция для хранения данных компонента</li>
+        <li>Это глобальный стейт</li>
+        <li>Это когда на ты никому не нужен</li>
+      </ul>
     </div>
   );
 }
