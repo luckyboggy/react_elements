82e1e8aca91f9a604599fa708e3f8b0f01380f31
Ковалёв Богдан Олегович <kovalev-bo@ais-npp.local>
2022-10-28 10:17:11 +0500

    modal_v1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React, { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <div className="App">
+      <button onClick={() => setModalVisible(true)} className='modal_open_btn'>Открыть окно</button>
+      {
+        modalVisible && (<div className='overlay'>
+          <div className="modal">
+            <div className="modal_header">
+              <div className="modal_title">модальное окно</div>
+              <div onClick={() => setModalVisible(false)} className="modal_close">&times;</div>
+            </div>
+            <div className='modal_content'>
+              <svg>
+                <title />
+                <path />
+              </svg>
+            </div>
+
+          </div>
+        </div>)
+      }
 
     </div>
   );
